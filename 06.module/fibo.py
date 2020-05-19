d227a6f17a3bebb7f52203cb0586e2d9cc4dd0a4
niiuchi <niiuchi@fork.co.jp>
2020-05-19 15:41:09 +0900

    update: add folders

--- 06.module/fibo.py
+++ 06.module/fibo.py
@@ -14,3 +14,9 @@ def fib2(n):
     result.append(a)
     a, b = b, a + b
   return result
+
+def display_str():
+  yes_votes = 42_572_654
+  no_votes = 43_132_495
+  percentage = yes_votes / (yes_votes + no_votes)
+  '{:-9} YES votes  {:2.2%}'.format(yes_votes, percentage)
